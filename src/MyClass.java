7b2424a8009ddf8c0ec1c7362fbffa27edf0de93
Cornboi <CorneliusMichaelD@gmail.com>
2021-09-29 14:22:52 -0500

    This is my feature update!

--- src/MyClass.java
+++ src/MyClass.java
@@ -3,7 +3,8 @@ public class MyClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("This is my first statement in this group");
+		System.out.println("This is my first statement in this group");
+		System.out.println("This is my new feature change!");
 	}
 
 }
